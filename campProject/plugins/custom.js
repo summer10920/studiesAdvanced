3473cd65abffdd94f72f1a7237b4eaa22f8493f5
Loki Jiang <37170364+summer10920@users.noreply.github.com>
2024-12-08 16:44:00 +0800

    20241208

--- campProject/plugins/custom.js
+++ campProject/plugins/custom.js
@@ -1,4 +1,67 @@
 onload = () => {
   var grid = document.querySelector('#lokiPark article.row');
   var msnry = new Masonry(grid, { percentPosition: 'true' });
+
+
+  //////////////////////////////////////////////////////////////////////// cookie 設定
+
+  // 1. 取得 cookieAry, ary value ex: cookieUsed=agree
+  const
+    aryCookie = document.cookie.split('; '),
+    nodeCookie = document.querySelector('#lokiCookie'),
+    keywordCookie = 'cookieUsed=agree';
+
+  // console.log(keywordCookie, aryCookie);
+
+  // 2. 檢查 cookieAry 是否有關鍵字
+  if (aryCookie.includes(keywordCookie)) {
+    // 已同意
+    nodeCookie.remove();
+  } else {
+    //還沒同意
+    nodeCookie.style.display = 'block'; // 顯示提示
+
+    nodeCookie.querySelector('button').onclick = () => { // 規劃按鈕事件
+      // 設定 cookie
+      // document.cookie = keywordCookie;   // method 1 沒指定
+
+      // method 2，指定活到甚麼時候
+      // now = new Date();
+      // now.setTime(now.getTime() + (24 * 60 * 60 * 1000));
+      // document.cookie = `${keywordCookie}; expires='${now.toUTCString()}`;
+
+      // method 3， 指定活多久
+      document.cookie = `${keywordCookie}; max-age=${24 * 60 * 60 * 180}`;
+      nodeCookie.remove();
+    };
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
--- campProject/plugins/lokiCalendar.js
+++ campProject/plugins/lokiCalendar.js
@@ -10,14 +10,14 @@ let
   pallet = {},
   myCalendar = null,
   tableData = { // 表格資料，到時候提供 tableMaker 輸出畫面，若要更新value，就對這裏修改
-    totalPrice: 999, //總價格
-    normalCount: 1, //平日幾晚
-    holidayCount: 5, //價日幾晚
+    totalPrice: 0, //總價格
+    normalCount: 0, //平日幾晚
+    holidayCount: 0, //價日幾晚
     pallet: { //營位資料 => 標題名稱、可賣數量、預約日金、小計、訂購數
       aArea: { title: '河畔 × A 區', sellCount: 0, sellInfo: '<div></div>', sumPrice: 0, orderCount: 0 },
-      bArea: { title: '山間 × B 區', sellCount: 6, sellInfo: '<div></div>', sumPrice: 0, orderCount: 0 },
-      cArea: { title: '平原 × C 區', sellCount: 7, sellInfo: '<div></div>', sumPrice: 0, orderCount: 0 },
-      dArea: { title: '車屋 × D 區', sellCount: 8, sellInfo: '<div></div>', sumPrice: 0, orderCount: 0 }
+      bArea: { title: '山間 × B 區', sellCount: 0, sellInfo: '<div></div>', sumPrice: 0, orderCount: 0 },
+      cArea: { title: '平原 × C 區', sellCount: 0, sellInfo: '<div></div>', sumPrice: 0, orderCount: 0 },
+      dArea: { title: '車屋 × D 區', sellCount: 0, sellInfo: '<div></div>', sumPrice: 0, orderCount: 0 }
     }
   };
 
@@ -33,8 +33,10 @@ const init = () => {
       ({ booked, pallet, nationalHoliday } = json);
 
       myCalendar = runCalendarService(); // 你創造一個服務原生函式，他提供一些method，像是 print, add, sub
-
       myCalendar.print(); // 對這個原生函式調用 print，產生DOM
+
+      document.querySelector('#selectPallet button').disabled = true;
+
 
       //規劃DOM事件
       document.querySelector('a[href="#nextCtrl"]').onclick = (e) => {  // 差，用HTML屬性 click 來綁定 JS 函式
@@ -46,7 +48,112 @@ const init = () => {
         myCalendar.sub();
       });
 
-      myCalendar.tableRefresh();
+      const nodeSelects = document.querySelectorAll('select');  // 四個下拉選單 [改變選項]的事件
+      nodeSelects.forEach(nodeSelect => { // 每個下拉選單個別發生事件時，都要重0計算總價
+        nodeSelect.onchange = (e) => {
+          tableData.totalPrice = 0;
+          nodeSelects.forEach(item => { // 總價就是 當下畫面的4組相加 (下拉數量 * 小計 sumPrice)
+            tableData.totalPrice += parseInt(item.value) * tableData.pallet[item.name].sumPrice;
+
+            // 更新 tableData 的 四組 orderCount，方便下一步驟可以直接獲取當下的選擇情況(不用再 DOM去找 select value)。
+            tableData.pallet[item.name].orderCount = parseInt(item.value);
+          });
+
+          // 事件最後，要更新畫面上的總價格，但不需要整個tablePrint(會大更新)，只需要更新html上面的小範圍就好
+          document.querySelector('#selectPallet h3').textContent = `
+            $${tableData.totalPrice} / ${tableData.normalCount}晚平日，${tableData.holidayCount}晚假日
+          `;
+
+          // 如果0元訂單，就不開放立即預約的按鈕
+          document.querySelector('#selectPallet button').disabled = tableData.totalPrice === 0;
+        }
+      });
+
+      document.querySelector('#selectPallet button').onclick = (e) => {   // 點擊[立即預約]的按鈕
+        //將tableData 想辦法整理到彈窗html上，接著呼喊 orderOffcanvas 出現
+        const orderOffcanvas = new bootstrap.Offcanvas('.offcanvas'); //左側談窗的bootstrap建構函式，可以操作動作
+        const nodeOffcanvas = document.querySelector('#orderForm'); // 左側談窗的html元素
+        let liStr = '';
+
+        // 將 tableData 四組資料跑出來
+        for (const key in tableData.pallet) {
+          if (tableData.pallet[key].orderCount === 0) continue;
+
+          // 如果走到這，代表有選擇1以上，接著我們整合到 liStr
+          liStr += `
+            <li class="list-group-item d-flex justify-content-between align-items-start">
+              <div class="ms-2 me-auto">
+                <div class="fw-bold">${tableData.pallet[key].title}</div>
+                <div>
+                  ${tableData.pallet[key].sellInfo}
+                </div>
+              </div>
+              <span class="badge bg-warning rounded-pill">x <span class="fs-6">${tableData.pallet[key].orderCount}</span>帳</span>
+            </li>
+          `;
+        }
+
+        nodeOffcanvas.querySelector('ol').innerHTML = liStr;
+        nodeOffcanvas.querySelector('.card-header.h5').textContent = document.querySelector('#selectPallet h3').textContent;
+        orderOffcanvas.show();
+      }
+
+      // offcanvas [提交訂單]的事件
+      // document.querySelector('#orderForm').onsubmit = (e) => {   // method 1
+      document.forms.orderForm.onsubmit = (e) => {  // method 2
+        e.preventDefault(); // 阻擋 html from 遇到 submit 會發生指向action動作，這時候要阻擋預設行為
+
+
+        // 1. 客製化表單資料，除了原本form 3組，多手動加2組
+        const sendData = new FormData(e.target);
+
+        // 手動兩個表單欄位，擴增到此sendData。就不需要在html上面做隱藏欄位。
+        // const selectDate = ["2024-12-11", "2024-12-12"];
+        const selectDate = [...document.querySelectorAll('li.selectHead, li.selectConnect')].map(i => i.dataset.date);
+        sendData.append('selectDate', JSON.stringify(selectDate)); // value 必須是一個JSON字串，html表單沒有object(array)這種值
+
+        // ex: const sellout = { "aArea": 2, "bArea": 2, "cArea": 0, "dArea": 4 };  //目標產生這樣的JSON字串塞入FormData
+        const sellout = {}; // 初始空陣列，慢慢塞回去
+        // ['aArea', 'bArea', 'cArea', 'dArea'].forEach(key => { // method 1
+        Object.keys(tableData.pallet).forEach(key => {  // method 2
+          sellout[key] = tableData.pallet[key].orderCount
+        })
+        sendData.append('selectDate', JSON.stringify(sellout)); // value 必須是一個JSON字串，html表單沒有object這種值
+
+        // 雖然透過console.log看不到formData內容，但可以用此方法檢查
+        // for (const [key, value] of sendData) {
+        //   console.log(key, value);
+        // }
+
+        // 2. 驗證表單有效性
+        if (!e.target.checkValidity()) e.target.classList.add('was-validated'); // 使用 bootstrap 的驗證功能
+        else {
+          // 3. 送出表單
+          fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: sendData,
+            // body: JSON.stringify({ userName: 1, password: 2 }),
+            // headers: { 'Content-Type': 'application/json' }
+          }).then(response => response.json())
+            .then(res => {
+              if (res.id) {
+                alert('感謝您的預約，您的訂單編號為:' + res.id);
+                document.location.reload();
+              }
+            });
+        }
+      };
+
+
+      // userName: 訪客姓名
+      // userPhone: 聯絡手機
+      // userMail: 123@123
+      // selectDate: ["2024-12-11","2024-12-12"]
+      // sellout: {"aArea":2,"bArea":2,"cArea":0,"dArea":4}
+
+
+
+      myCalendar.tableRefresh(); // 網頁載入的第一次 tablePrint
     });
 
 }
@@ -68,6 +175,7 @@ const runCalendarService = () => {
   const
     today = dayjs(),
     userChooseDays = [null, null],
+    InitTableDataStr = JSON.stringify(tableData), // 轉為普通字串，脫離物件導向觀念
     changeMonth = (num) => { // 先歸零，重新計算該有的 title 跟 listBox
       theDay = theDay.add(num, 'M'); // 今天的時間物件，透過第三方套件獲取
       calLeft = {
@@ -196,8 +304,48 @@ const runCalendarService = () => {
       })
     },
     tableMaker = () => {
-      console.log('整理 tableData');
-      // tablePrint();
+      // 負責翻新 全域變數的tableData
+      tableData = JSON.parse(InitTableDataStr); // 利用字串轉物件，會整個產生一個新的物件，絕對跟原本的物件不一樣。
+
+      //1. 修正 sellCount，先取得total總數，之後再根據訂單一個個減少
+      for (const key in tableData.pallet) { // 獲得四組pallet名字，回頭對 tableData 修改 sellCount
+        tableData.pallet[key].sellCount = pallet[key].total; // 10
+      }
+
+      //2. 去得知user 選啥 AB 日期
+      document.querySelectorAll('li.selectHead, li.selectConnect').forEach(nodeLi => {
+        // console.log(nodeLi.dataset.date); // 2024-12-13
+        for (const key in tableData.pallet) { // 獲取四個 pallet 名字
+          // const hasOrder = booked.find(bookItem => {
+          //   return bookItem.date === nodeLi.dataset.date;
+          // });
+          const hasOrder = booked.find(bookItem => bookItem.date === nodeLi.dataset.date); // arrow fn is more faster
+
+          // 2-1. 如果後端有找到當日的訂單，更新房況剩餘數
+          if (hasOrder) {
+            // 在連續的訂單日子，可以賣給客人的房數必須是這些剩餘房況的最小值
+            tableData.pallet[key].sellCount = Math.min(tableData.pallet[key].sellCount, pallet[key].total - hasOrder.sellout[key]);
+          }
+
+          // 2-2.如果房況剩餘數的結論為:房況有剩，提供該key的 sellInfo 販售資訊 (日期 /每帳價格)；沒剩就顯示已售完
+          if (tableData.pallet[key].sellCount) {
+            // const dayPrice = nodeLi.classList.contains('holiday') ? pallet[key].holidayPrice : pallet[key].normalPrice; //method 1
+            const dayPrice = pallet[key][nodeLi.classList.contains('holiday') ? 'holidayPrice' : 'normalPrice']; //method 2
+
+            // console.log(nodeLi.dataset.date, dayPrice); // 日期
+            tableData.pallet[key].sellInfo += `<div>${nodeLi.dataset.date} (${dayPrice})</div>`;
+            tableData.pallet[key].sumPrice += dayPrice;
+          } else {
+            tableData.pallet[key].sellInfo = `<div>已售完</div>`;
+            tableData.pallet[key].sumPrice = 0;
+          }
+        }
+
+        // 2-3. 根據 user 選的日期，判斷有沒有class holiday，疊加假日或平日數量
+        // nodeLi.classList.contains('holiday') ? tableData.holidayCount++ : tableData.normalCount++; //method 1
+        tableData[nodeLi.classList.contains('holiday') ? 'holidayCount' : 'normalCount']++; //method 2
+      });
+      tablePrint(); // 因為user choose A B 日期，觸發的 tablePrint (這裡會對全域變數的 tableData 進行刷新畫面輸出)
     },
     tablePrint = () => {
       document.querySelectorAll('#selectPallet select').forEach(nodeSelect => {
