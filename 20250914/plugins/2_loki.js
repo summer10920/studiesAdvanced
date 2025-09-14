e60d96177b505435106fb620cc6bde3dcecebd9c
Loki Jiang <37170364+summer10920@users.noreply.github.com>
2025-09-14 16:20:14 +0800

    20250914 end

---
+++ 20250914/plugins/2_loki.js
@@ -0,0 +1,14 @@
+shopCartPrice = 200;
+/* 
+ * 註解化，不會被瀏覽器執行
+ * 可以拿來寫筆記，註解給自己這行要做甚麼
+ * 分為多行註解跟單行註解兩種符號表示法
+ * 註解也可以拿來做暫時性的忽略測試
+ */
+// console.log(123);
+// 要求瀏覽器執行 console 輸出
+console.log(shopCartPrice);
+console.warn("hello world，你好世界!!");   // 產生 warning 效果的訊息輸出 
+console.error("hello world，你好世界!!"); // 產生 error 效果的訊息輸出 
+// console.info("hello world，你好世界!!");
+console.log("hello world，你好世界!!");
